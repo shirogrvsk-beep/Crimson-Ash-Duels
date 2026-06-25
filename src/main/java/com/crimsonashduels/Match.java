6e19a514db5d7f081ea709028865ece2ae7124cc
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-26 00:27:51 +0800

    Match.java

--- src/main/java/com/crimsonashduels/Match.java
+++ src/main/java/com/crimsonashduels/Match.java
@@ -6,14 +6,17 @@ public class Match {
     private final Player player1;
     private final Player player2;
     private final Arena arena;
+    private final String kitName;
 
-    public Match(Player player1, Player player2, Arena arena) {
+    public Match(Player player1, Player player2, Arena arena, String kitName) {
         this.player1 = player1;
         this.player2 = player2;
         this.arena = arena;
+        this.kitName = kitName;
     }
 
     public Player getPlayer1() { return player1; }
     public Player getPlayer2() { return player2; }
     public Arena getArena() { return arena; }
+    public String getKitName() { return kitName; }
 }
