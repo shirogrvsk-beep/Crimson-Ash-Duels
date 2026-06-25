17249b55bda43c480a8fbbfcc203cef75bf07c66
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-25 21:46:38 +0800

    TopWinsGUICommand.java

---
+++ src/main/java/com/crimsonashduels/commands/TopWinsGUICommand.java
@@ -0,0 +1,28 @@
+package com.crimsonashduels.commands;
+
+import com.crimsonashduels.CrimsonAshDuels;
+import com.crimsonashduels.LeaderboardGUI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TopWinsGUICommand implements CommandExecutor {
+
+    private final LeaderboardGUI gui;
+
+    public TopWinsGUICommand(CrimsonAshDuels plugin) {
+        this.gui = new LeaderboardGUI(plugin);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can view leaderboards.");
+            return true;
+        }
+        Player player = (Player) sender;
+        gui.openWinsLeaderboard(player);
+        return true;
+    }
+}
