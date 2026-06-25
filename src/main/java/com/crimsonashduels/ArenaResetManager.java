5eb489f2219a8efbd8e5f47693e43cd0687d1462
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-26 00:17:26 +0800

    ArenaResetManager.java

--- src/main/java/com/crimsonashduels/ArenaResetManager.java
+++ src/main/java/com/crimsonashduels/ArenaResetManager.java
@@ -1,22 +1,26 @@
 package com.crimsonashduels;
 
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
-import com.sk89q.worldedit.session.ClipboardHolder;
-import com.sk89q.worldedit.world.World;
-import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.session.ClipboardHolder;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ArenaResetManager {
-
     private final CrimsonAshDuels plugin;
 
     public ArenaResetManager(CrimsonAshDuels plugin) {
@@ -24,48 +28,49 @@ public class ArenaResetManager {
     }
 
     public void saveArena(String name, Location corner1, Location corner2) {
-        try {
-            World weWorld = BukkitAdapter.adapt(corner1.getWorld());
-            BlockVector3 min = BlockVector3.at(
-                    Math.min(corner1.getBlockX(), corner2.getBlockX()),
-                    Math.min(corner1.getBlockY(), corner2.getBlockY()),
-                    Math.min(corner1.getBlockZ(), corner2.getBlockZ())
-            );
-            BlockVector3 max = BlockVector3.at(
-                    Math.max(corner1.getBlockX(), corner2.getBlockX()),
-                    Math.max(corner1.getBlockY(), corner2.getBlockY()),
-                    Math.max(corner1.getBlockZ(), corner2.getBlockZ())
-            );
+        World world = corner1.getWorld();
+        BlockVector3 min = BlockVector3.at(
+                Math.min(corner1.getBlockX(), corner2.getBlockX()),
+                Math.min(corner1.getBlockY(), corner2.getBlockY()),
+                Math.min(corner1.getBlockZ(), corner2.getBlockZ())
+        );
+        BlockVector3 max = BlockVector3.at(
+                Math.max(corner1.getBlockX(), corner2.getBlockX()),
+                Math.max(corner1.getBlockY(), corner2.getBlockY()),
+                Math.max(corner1.getBlockZ(), corner2.getBlockZ())
+        );
 
-            File file = new File(plugin.getDataFolder(), name + ".schem");
-            ClipboardFormat format = ClipboardFormat.SCHEMATIC;
-            try (ClipboardWriter writer = format.getWriter(new FileOutputStream(file))) {
-                Clipboard clipboard = new com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard(
-                        new com.sk89q.worldedit.math.BlockVector3(min.getX(), min.getY(), min.getZ()),
-                        new com.sk89q.worldedit.math.BlockVector3(max.getX(), max.getY(), max.getZ())
-                );
-                writer.write(clipboard);
-            }
-        } catch (Exception e) {
+        Region region = new CuboidRegion(BukkitAdapter.adapt(world), min, max);
+        Clipboard clipboard = new BlockArrayClipboard(region);
+
+        File schemFile = new File(plugin.getDataFolder(), name + ".schem");
+        ClipboardFormat format = ClipboardFormats.findByFile(schemFile);
+
+        try (ClipboardWriter writer = format.getWriter(new FileOutputStream(schemFile))) {
+            writer.write(clipboard);
+            plugin.getLogger().info("Arena " + name + " saved to " + schemFile.getName());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void restoreArena(String name, Location pasteLocation) {
-        try {
-            File file = new File(plugin.getDataFolder(), name + ".schem");
-            ClipboardFormat format = ClipboardFormat.SCHEMATIC;
-            try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
-                Clipboard clipboard = reader.read();
-                World weWorld = BukkitAdapter.adapt(pasteLocation.getWorld());
-                try (EditSession editSession = com.sk89q.worldedit.WorldEdit.getInstance().newEditSession(weWorld)) {
-                    ClipboardHolder holder = new ClipboardHolder(clipboard);
-                    holder.createPaste(editSession)
-                            .to(BlockVector3.at(pasteLocation.getBlockX(), pasteLocation.getBlockY(), pasteLocation.getBlockZ()))
-                            .ignoreAirBlocks(false)
-                            .build();
-                }
-            }
+        File schemFile = new File(plugin.getDataFolder(), name + ".schem");
+        ClipboardFormat format = ClipboardFormats.findByFile(schemFile);
+
+        if (format == null) {
+            plugin.getLogger().warning("No schematic format found for arena " + name);
+            return;
+        }
+
+        try (EditSession editSession = WorldEdit.getInstance().newEditSession(BukkitAdapter.adapt(pasteLocation.getWorld()))) {
+            Clipboard clipboard = format.getReader(schemFile).read();
+            ClipboardHolder holder = new ClipboardHolder(clipboard);
+            holder.createPaste(editSession)
+                    .to(BlockVector3.at(pasteLocation.getBlockX(), pasteLocation.getBlockY(), pasteLocation.getBlockZ()))
+                    .ignoreAirBlocks(false)
+                    .build();
+            plugin.getLogger().info("Arena " + name + " restored at " + pasteLocation);
         } catch (Exception e) {
             e.printStackTrace();
         }
