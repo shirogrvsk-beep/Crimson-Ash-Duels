165945fa3ea5d7c094b5a19661a1a4a775212637
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-25 20:47:10 +0800

    MatchManager.java

---
+++ src/main/java/com/crimsonashduels/MatchManager.java
@@ -0,0 +1,31 @@
+package com.crimsonashduels;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MatchManager {
+
+    private final Map<Player, Match> activeMatches = new HashMap<>();
+
+    public void startMatch(Player p1, Player p2) {
+        Match match = new Match(p1, p2);
+        activeMatches.put(p1, match);
+        activeMatches.put(p2, match);
+    }
+
+    public boolean isInMatch(Player player) {
+        return activeMatches.containsKey(player);
+    }
+
+    public Match getMatch(Player player) {
+        return activeMatches.get(player);
+    }
+
+    public void endMatch(Match match, Player winner, Player loser) {
+        match.endMatch(winner, loser);
+        activeMatches.remove(match.getPlayer1());
+        activeMatches.remove(match.getPlayer2());
+    }
+}
