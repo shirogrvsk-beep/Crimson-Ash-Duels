40efcd3917ceaa559d1682601c40396948268d8f
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-25 20:29:17 +0800

    DuelCommand.java

---
+++ src/main/java/com/crimsonashduels/commands/DuelCommand.java
@@ -0,0 +1,37 @@
+package com.crimsonashduels.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DuelCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        Player challenger = (Player) sender;
+
+        if (args.length != 1) {
+            challenger.sendMessage("Usage: /duel <player>");
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            challenger.sendMessage("Player not found.");
+            return true;
+        }
+
+        challenger.sendMessage("You challenged " + target.getName() + " to a duel!");
+        target.sendMessage(challenger.getName() + " has challenged you to a duel! Type /duelaccept to fight.");
+
+        // TODO: Add DuelManager to track requests
+        return true;
+    }
+}
