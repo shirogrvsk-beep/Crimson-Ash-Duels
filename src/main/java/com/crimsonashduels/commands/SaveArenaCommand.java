caf8ebd54aeb8f24e2ee747913d00efe93e9a98b
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-26 00:09:33 +0800

    SaveArenaCommand.java

---
+++ src/main/java/com/crimsonashduels/commands/SaveArenaCommand.java
@@ -0,0 +1,20 @@
+package com.crimsonashduels.commands;
+
+import com.crimsonashduels.CrimsonAshDuels;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SaveArenaCommand implements CommandExecutor {
+    private final CrimsonAshDuels plugin;
+
+    public SaveArenaCommand(CrimsonAshDuels plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("Arena save not yet implemented.");
+        return true;
+    }
+}
