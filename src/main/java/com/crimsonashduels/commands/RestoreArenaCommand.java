4f370032fc04b12c01c9622cb955ed6fa0339c0e
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-26 00:10:00 +0800

    RestoreArenaCommand.java

---
+++ src/main/java/com/crimsonashduels/commands/RestoreArenaCommand.java
@@ -0,0 +1,20 @@
+package com.crimsonashduels.commands;
+
+import com.crimsonashduels.CrimsonAshDuels;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class RestoreArenaCommand implements CommandExecutor {
+    private final CrimsonAshDuels plugin;
+
+    public RestoreArenaCommand(CrimsonAshDuels plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("Arena restore not yet implemented.");
+        return true;
+    }
+}
