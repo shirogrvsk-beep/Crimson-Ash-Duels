e8056eebcbbfb1ae749719bea2dee0e8e7c161a9
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-25 20:32:21 +0800

    DuelAcceptCommand.java

---
+++ src/main/java/com/crimsonashduels/commands/DuelAcceptCommand.java
@@ -0,0 +1,45 @@
+package com.crimsonashduels.commands;
+
+import com.crimsonashduels.DuelManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DuelAcceptCommand implements CommandExecutor {
+
+    private final DuelManager duelManager;
+
+    public DuelAcceptCommand(DuelManager duelManager) {
+        this.duelManager = duelManager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can accept duels.");
+            return true;
+        }
+
+        Player target = (Player) sender;
+
+        if (!duelManager.hasRequest(target)) {
+            target.sendMessage("You have no pending duel requests.");
+            return true;
+        }
+
+        Player challenger = duelManager.getChallenger(target);
+        if (challenger == null) {
+            target.sendMessage("No challenger found.");
+            return true;
+        }
+
+        target.sendMessage("You accepted the duel against " + challenger.getName() + "!");
+        challenger.sendMessage(target.getName() + " accepted your duel!");
+
+        // TODO: Start duel logic (teleport to arena, clear inventory, etc.)
+        duelManager.removeRequest(challenger);
+
+        return true;
+    }
+}
